27d67b93058885cd15ec7b2e704e738c0f128ffe
marmadukeandbob <marmadukeandbob@gmail.com>
2018-03-05 22:08:00 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+$(function(){
+   $("#r1").click(function () {
+      $('#go').trigger('click');
+   });
+   $("#r2").click(function () {
+     $('#go').trigger('click');
+   });
+   $("#go").click(function () {
+
+
+
+     if ($('input[name=w-type]:checked').val() == "ex") {
+        var DOI = $("#DOI");
+        var good_url = ("//api.growkudos.com/widgets/article/".concat(DOI.val()));
+     }
+
+     if ($('input[name=w-type]:checked').val() == "re") {
+         var DOI = $("#DOI");
+         var good_url = ("//api.growkudos.com/widgets/resources/".concat(DOI.val()));
+     }
+
+     $("#preview").css("display", "inline");
+
+
+
+
+     var division = document.getElementById("preview");
+     while (division.firstChild) {
+        division.removeChild(division.firstChild);
+     }
+
+     var newItem = document.createElement('script');
+     newItem.setAttribute("type","text/javascript");
+     newItem.setAttribute("src", good_url);
+
+     division.insertBefore(newItem, division.childNodes[0]);
+
+
+   });
+});
